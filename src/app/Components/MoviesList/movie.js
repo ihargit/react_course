09e9df5ff9ef613011d7b1b5112b5a239d01d9fa
Ihar Kharashun <ihar.kharashun@gmail.com>
2021-03-19 23:40:46 +0300

    fix: movie-three-dots-menu

--- src/app/Components/MoviesList/movie.js
+++ src/app/Components/MoviesList/movie.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import PropTypes from 'prop-types';
 import ErrorBoundary from '../ErrorBoundary';
 import Movies from './movies';
 
 const Movie = ({ url, title, genre, releaseDate }) => {
+  const [hidden, setHidden] = useState('hide');
+  const toggleHidden = () => setHidden( hidden ? '' : 'hide');
   return (
     <ErrorBoundary>
       <div className="movie">
+        <button className="movie-three-dots-icon hide" onClick={toggleHidden}></button>
+        <div className={`movie-three-dots-menu ${hidden}`}>
+          <button>x</button>
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
         <img className="movie-image" src={url} />
         <div className="movie-description">
           <p className="movie-title">{title}</p>
